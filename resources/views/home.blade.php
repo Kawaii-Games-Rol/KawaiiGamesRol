92817e23c586ee805520c48f30d84bc466ddf18e
skeret2 <90117547+skeret2@users.noreply.github.com>
2021-10-12 18:54:47 -0300

    Botones tabla

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Has iniciado sesion!') }}
                 </div>
             </div>
         </div>
