444707d1a94d27e4ac32f1b2f01152ca598e8cc0
FlaminEon <aeonf4@gmail.com>
2021-10-16 17:50:27 -0300

    agregado el codigo de ValidarRut

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use App\Rules\ValidarRut;
 
 class RegisterController extends Controller
 {
@@ -52,7 +53,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'rut'=> ['required', 'string', 'unique:users'],
+            'rut'=> ['required', 'string', 'unique:users', new ValidarRut()],
             'rol' => ['string','required', 'in:Administrador,Jefe Carrera,Alumno'],
             'carrera'=>['exists:App\Models\Carrera,id']
 
--- app/Rules/ValidarRut.php
+++ app/Rules/ValidarRut.php
@@ -25,9 +25,7 @@ class ValidarRut implements Rule
      */
     public function passes($attribute, $value)
     {
-        /* Logica para validar rut */
-
-        return true;
+        return ($this->valida_rut($value));
     }
 
     /**
@@ -37,6 +35,38 @@ class ValidarRut implements Rule
      */
     public function message()
     {
-        return 'El rut no es válido.';
+        return 'El rut no es valido.';
+    }
+
+    public function valida_rut($rut)
+    {
+        if (!preg_match("/^[0-9.]+[-]?+[0-9kK]{1}/", $rut)) {
+            return false;
+        }
+        if (str_contains($rut, '.') || str_contains($rut, '-')){
+            return false;
+        }
+
+        $rut = preg_replace('/[\.\-]/i', '', $rut);
+        $dv = substr($rut, -1);
+        $numero = substr($rut, 0, strlen($rut) - 1);
+        $i = 2;
+        $suma = 0;
+        foreach (array_reverse(str_split($numero)) as $v) {
+            if ($i == 8)
+                $i = 2;
+            $suma += $v * $i;
+            ++$i;
+        }
+        $dvr = 11 - ($suma % 11);
+
+        if ($dvr == 11)
+            $dvr = 0;
+        if ($dvr == 10)
+            $dvr = 'K';
+        if ($dvr == strtoupper($dv))
+            return true;
+        else
+            return false;
     }
 }
