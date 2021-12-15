28c5f9a1cee6e061404c1a5ef86837f5b615c8f7
invadux <hanshennings18@gmail.com>
2021-12-15 20:59:44 -0300

    dsad
    asdasd

--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,7 @@ Route::middleware(['rutasAlumno'])->group(function () {
 
 Route::middleware(['rutasJefeCarrera'])->group(function () {
     Route::get('buscarEstudiante', function(){return view('auth.buscarEstudiante.index');})->name('buscarEstudiante');
+    Route::get('GestionSolicitud', function(){return view('GestionSolicitud.index');})->name('GestionSolicitud');
     Route::get('filtrarSolicitudes', function(){return view('filtrarSolicitudes.index');})->name('filtarSolicitudes');
     Route::post('alumno',[BuscarEstudianteController::class, 'devolverEstudiante'])->name('postBuscarEstudiante');
     Route::get('alumno/{id}', [BuscarEstudianteController::class,'mostrarEstudiante'])->name('mostrarEstudiante');
