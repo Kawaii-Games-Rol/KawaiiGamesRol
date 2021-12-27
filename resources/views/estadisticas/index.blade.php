39de908e8c8a2515d5c26d7556127768ece04795
FlaminEon <aeonf4@gmail.com>
2021-12-27 20:28:02 -0300

    Quitado cuadros de fecha duplicados

--- resources/views/estadisticas/index.blade.php
+++ resources/views/estadisticas/index.blade.php
@@ -3,10 +3,6 @@
 @section('content')
 
 <div class="container">
- <p class="text">Desde:</p>
-    <input type="date">
-    <div id="Fecha"   p class="text">Hasta:</p>
-    <input type="date">
     <h1 style="font-size: 50px" class="text-center">Estadísticas del sistema</h1>
     <div class="row row-cols-1 row-cols-md-3">
         <div class="col mb-4">
@@ -29,11 +25,11 @@
                     <h5 class="card-title">Cantidad Total de Solicitudes: {{$cantEnRango}}</h5>
                 </div>
             </div>
-                       
+
         </div>
-        
+
     </div>
-    
+
 </div>
 <div class="col-lg-12 login-form">
                 <div class="col-lg-12 login-form">
@@ -51,7 +47,7 @@
                                 <strong>{{ $message }}</strong>
                             </span>
                             @enderror
-                        </div>          
+                        </div>
                         <div class="container" >
                             <p class="text">>Hasta</p>
                              <input type="date"
@@ -63,8 +59,8 @@
                                 <strong>{{ $message }}</strong>
                             </span>
                             @enderror
-                        </div>     
-                      
+                        </div>
+
                         <form id="formulario"
                             method="POST"
                             action="{{ route('postfiltrarSolicitud') }}">
@@ -77,7 +73,7 @@
                                     name="Hola"
                                     value="{{ old('numero') }}"
                                     required
-                                
+
                                     autofocus>
 
                                 @error('rut')
